6039a68369622160130ff9aca71d9535f896e4e9
Ivan Scoropad <vaneawar@gmail.com>
2020-12-26 10:32:08 +0200

    Add project files.

---
+++ GitSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
